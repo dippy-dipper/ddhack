8babd16c5d9578f00ba079548fed245a6a9dfe1c
dippy-dipper <70149011+dippy-dipper@users.noreply.github.com>
2021-01-19 13:14:07 +0200

    MinGW compatibility changes

--- logger/myIDDraw2.cpp
+++ logger/myIDDraw2.cpp
@@ -1,7 +1,7 @@
 #include "StdAfx.h"
-#include <varargs.h>
+#include <stdarg.h>
 
-
+void logf(void * thisptr, const char *msg, ...);
 
 myIDDraw2::myIDDraw2(LPDIRECTDRAW2 pOriginal)
 {
@@ -19,7 +19,7 @@ myIDDraw2::~myIDDraw2(void)
 HRESULT __stdcall myIDDraw2::QueryInterface (REFIID a, LPVOID FAR * b)
 {
 	
-	char *iid = "?";
+	const char *iid = "?";
 	if (a == IID_IDirectDraw) iid = "IID_IDirectDraw";
 	if (a == IID_IDirectDraw2) iid = "IID_IDirectDraw2";
 	if (a == IID_IDirectDraw4) iid = "IID_IDirectDraw4";
