b2aa9dda75a63579881bfeb70a24e8b791b46f06
dippy-dipper <70149011+dippy-dipper@users.noreply.github.com>
2021-01-19 13:02:45 +0200

    Add files via upload

---
+++ hack/Make.cmd
@@ -0,0 +1,2 @@
+g++ -Wl,--enable-stdcall-fixup -Ilibs/include -O2 -shared -s -o ddraw_hack.dll ddhack.cpp myIDDraw1.cpp myIDDraw2.cpp myIDDrawPalette.cpp myIDDrawSurface1.cpp stdafx.cpp ddhack.def -L. -lgdi32 -lddraw -ldxguid -lopengl32 -static-libgcc
+pause
