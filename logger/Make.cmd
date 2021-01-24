d17574f06dc8d60ae92951e26f6537b2aeb2a8e3
dippy-dipper <70149011+dippy-dipper@users.noreply.github.com>
2021-01-25 01:05:49 +0200

    statically link libstdc++

--- logger/Make.cmd
+++ logger/Make.cmd
@@ -1,2 +1,2 @@
-g++ -Wl,--enable-stdcall-fixup -Ilibs/include -O2 -shared -s -o ddraw_logger.dll ddwrapper.cpp myIDDraw1.cpp myIDDraw2.cpp myIDDraw4.cpp myIDDraw7.cpp myIDDrawPalette.cpp myIDDrawSurface1.cpp myIDDrawSurface2.cpp myIDDrawSurface3.cpp myIDDrawSurface4.cpp myIDDrawSurface7.cpp stdafx.cpp ddwrapper.def -L. -lgdi32 -lddraw -ldxguid -lopengl32 -static-libgcc
+g++ -Wl,--enable-stdcall-fixup -Ilibs/include -O2 -shared -s -o ddraw_logger.dll ddwrapper.cpp myIDDraw1.cpp myIDDraw2.cpp myIDDraw4.cpp myIDDraw7.cpp myIDDrawPalette.cpp myIDDrawSurface1.cpp myIDDrawSurface2.cpp myIDDrawSurface3.cpp myIDDrawSurface4.cpp myIDDrawSurface7.cpp stdafx.cpp ddwrapper.def -L. -lgdi32 -lddraw -ldxguid -lopengl32 -static-libgcc -static-libstdc++
 pause
